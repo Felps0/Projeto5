c60c7c0d229ace0dd5587cb154befef2643130f3
Técnico em Informática <24121742018.1@senacrjedu.local>
2020-01-15 20:43:39 -0300

    15/01/2020

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Router } from '@angular/router';
 import { MensagemService } from 'src/app/services/mensagem.service';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Platform } from '@ionic/angular';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -11,11 +15,13 @@ export class LoginPage implements OnInit {
 
   protected email: string;
   protected senha: string;
-  protected msg: MensagemService;
 
   constructor(
     public afAuth: AngularFireAuth,
     protected router: Router,
+    protected msg: MensagemService,
+    private googlePlus: GooglePlus,
+    private platform: Platform
   ) { }
 
   ngOnInit() {
@@ -23,6 +29,14 @@ export class LoginPage implements OnInit {
 
   onsubmit(form) {
     this.login()
+  }
+
+  loginWEB() {
+    if (this.platform.is("cordova")) {
+      this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    } else {
+      this.loginGooglePlus()
+    }
   }
 
   login() {
@@ -36,5 +50,20 @@ export class LoginPage implements OnInit {
         this.msg.presentAlert("Ops!", "E-mail e/ou senha invalida!");
       }
     )
- }
+  }
+
+  loginGooglePlus() {
+    this.googlePlus.login({})
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+    if (!this.platform.is("cordova")){
+      this.googlePlus.logout()
+    }
+    this.router.navigate(['/']);
+  }
+
 }
